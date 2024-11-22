82c6eb78a24cf41f27b2403aeb8acee8f87ad9eb
Gurov Denis <GurovD@mos.rusbiopharm.ru>
2024-11-22 15:15:27 +0300

    правки в ссылках

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,7 +3,6 @@ import io
 from django.db.models import Sum
 from django.http import HttpResponse
 from django.shortcuts import get_object_or_404, redirect
-from django.urls import reverse
 
 from django_filters.rest_framework import DjangoFilterBackend
 from reportlab.lib.pagesizes import letter
